d618b8fcfe6190616823facb437c0a53b962cd37
Denys Piuro <denys.pyuro@gmail.com>
2024-09-10 13:32:31 +0300

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,15 +1,38 @@
 from django.shortcuts import render
-
-from taxi.models import Driver, Car, Manufacturer
+from django.views.generic import ListView, DetailView
+from .models import Car, Driver, Manufacturer
 
 
 def index(request):
-    """View function for the home page of the site."""
-
     context = {
         "num_drivers": Driver.objects.count(),
         "num_cars": Car.objects.count(),
         "num_manufacturers": Manufacturer.objects.count(),
     }
-
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.all().order_by("name")
+    paginate_by = 5
+
+
+class CarListView(ListView):
+    model = Car
+    paginate_by = 5
+    queryset = Car.objects.select_related("manufacturer")
+
+
+class CarDetailView(DetailView):
+    model = Car
+
+
+class DriverListView(ListView):
+    model = Driver
+    paginate_by = 5
+
+
+class DriverDetailView(DetailView):
+    model = Driver
+    queryset = Driver.objects.prefetch_related("cars")
